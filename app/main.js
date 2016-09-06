22c09adae51f31db6369d05f4ef2d0fcde310fb7
Amit Prakash Singh <amit.prakashsingh@gmail.com>
2016-09-06 17:24:42 +0530

    Setup Angular 2 Project running

---
+++ app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_js_1 = require('./app.module.js');
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(app_module_js_1.AppModule);
+//# sourceMappingURL=main.js.map
